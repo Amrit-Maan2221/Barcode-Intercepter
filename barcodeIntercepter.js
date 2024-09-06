d53cf2f2482e7a2ea11eb6be233b9f9ff027d8ea
Amritpal Singh <83029875+Amrit-Maan2221@users.noreply.github.com>
2024-09-06 17:15:56 -0400

    Update barcodeIntercepter.js

--- barcodeIntercepter.js
+++ barcodeIntercepter.js
@@ -1,4 +1,13 @@
-/**
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+
+
+        /**
  * Detects whether the scanned input is a valid Barcode.
  * 
  * @param {Object} args - An object containing the necessary parameters for barcode detection.
@@ -32,16 +41,120 @@ function barcodeIntercepter(args) {
     let toggleScannerBtn;
     appendToogleBtn();
     toggleScanner();
+    addScannerOnlyModeBtn();
 
     document.addEventListener('focusin', handleWindowFocus);
     document.addEventListener('click', handleWindowFocus);
     document.addEventListener('keyup', handleWindowFocus);
-    
+
     handleIFrames();
 
 
     addEventListener("keydown", handleInput);
-    
+
+    function GetIndexForScannerOnlyMode() {
+        let zIndexes = Array.from(document.querySelectorAll('body *'))
+            .map(a => parseFloat(window.getComputedStyle(a).zIndex))
+            .filter(a => !isNaN(a))
+            .sort((a, b) => a < b == true ? -1 : 1);
+        let index = zIndexes[zIndexes.length - 1];
+        return index;
+    }
+
+    function addScannerOnlyModeBtn() {
+        const style = document.createElement('style');
+        const zIndex = GetIndexForScannerOnlyMode();
+        style.innerHTML = `
+            .scanner_btn {
+                position: fixed;
+                width: 25px;
+                height: 25px;
+                bottom: 0px;
+                left: 0px;
+                background-color: black;
+                color: white;
+                border-radius: 50px;
+                text-align: center;
+                box-shadow: 2px 2px 3px #999;
+                z-index: ${zIndex};
+                transition-duration: 200ms;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+
+            .scanner_btn span {
+                font-size: 21px;
+            }
+
+            .scanner_btn:hover {
+                font-family: 'Material Icons';
+                cursor: pointer;
+                transform: scale(1.5);
+                transform-origin: bottom left;
+            }
+
+            .scanner_btn.mode_on {
+                background-color: #2c7b30;
+                color: #fff;
+            }
+
+
+            .scanner_icon_div {
+                text-align: center;
+                vertical-align: middle;
+                font-size: 70px;
+                color: #1100ff26;
+            }
+
+            .scanner_icon {
+                text-align: center;
+                vertical-align: middle;
+                font-size: 70px;
+                color: #1100ff26;
+            }
+        `;
+        document.head.appendChild(style);
+
+        // Create the scanner button element
+        const scannerModeBtn = document.createElement('span');
+        scannerModeBtn.id = "scannerModeBtn";
+        scannerModeBtn.className = "scanner_btn";
+        scannerModeBtn.title = "Scanner Only Mode";
+        scannerModeBtn.tabIndex = 0;
+        scannerModeBtn.accessKey = "b";
+        scannerModeBtn.onclick = function () {
+            const html = `
+                <div id='BM-scannerMode' class='scannerModeDiv' tabindex='0' style='position: fixed; z-index: 1050; top: 0; left: 0; width: 100%; height: 100%; background-color: #a1b6c942; display: flex; align-items: center; justify-content: center;'>
+                    <span class='scanner_icon_div'>
+                        <svg fill="#000000" height="200px" width="200px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 60 60" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M5,49c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v37C4,48.553,4.448,49,5,49z"></path> <path d="M55,49c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v37C54,48.553,54.448,49,55,49z"></path> <path d="M9,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C8,40.553,8.448,41,9,41z"></path> <path d="M12,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C11,40.553,11.448,41,12,41z"></path> <path d="M17,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C16,40.553,16.448,41,17,41z"></path> <path d="M20,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C19,40.553,19.448,41,20,41z"></path> <path d="M23,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C22,40.553,22.448,41,23,41z"></path> <path d="M29,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C28,40.553,28.448,41,29,41z"></path> <path d="M34,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C33,40.553,33.448,41,34,41z"></path> <path d="M37,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C36,40.553,36.448,41,37,41z"></path> <path d="M42,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C41,40.553,41.448,41,42,41z"></path> <path d="M45,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C44,40.553,44.448,41,45,41z"></path> <path d="M51,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C50,40.553,50.448,41,51,41z"></path> <path d="M9,47c0.552,0,1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1v1C8,46.553,8.448,47,9,47z"></path> <path d="M12,47c0.552,0,1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1v1C11,46.553,11.448,47,12,47z"></path> <path d="M15,47c0.552,0,1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1v1C14,46.553,14.448,47,15,47z"></path> <path d="M17,45v1c0,0.553,0.448,1,1,1s1-0.447,1-1v-1c0-0.553-0.448-1-1-1S17,44.447,17,45z"></path> <path d="M20,45v1c0,0.553,0.448,1,1,1s1-0.447,1-1v-1c0-0.553-0.448-1-1-1S20,44.447,20,45z"></path> <path d="M23,45v1c0,0.553,0.448,1,1,1s1-0.447,1-1v-1c0-0.553-0.448-1-1-1S23,44.447,23,45z"></path> <path d="M27,47c0.552,0,1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1v1C26,46.553,26.448,47,27,47z"></path> <path d="M29,45v1c0,0.553,0.448,1,1,1s1-0.447,1-1v-1c0-0.553-0.448-1-1-1S29,44.447,29,45z"></path> <path d="M33,47c0.552,0,1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1v1C32,46.553,32.448,47,33,47z"></path> <path d="M35,46c0,0.553,0.448,1,1,1s1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1V46z"></path> <path d="M38,46c0,0.553,0.448,1,1,1s1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1V46z"></path> <path d="M42,47c0.552,0,1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1v1C41,46.553,41.448,47,42,47z"></path> <path d="M45,47c0.552,0,1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1v1C44,46.553,44.448,47,45,47z"></path> <path d="M48,47c0.552,0,1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1v1C47,46.553,47.448,47,48,47z"></path> <path d="M51,47c0.552,0,1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1v1C50,46.553,50.448,47,51,47z"></path> <path d="M0,5v50h60V5H0z M58,53H2V7h56V53z"></path> </g> </g></svg> Scanner Only Mode
+                    </span>
+                </div>
+            `;
+
+            if (!document.querySelector("#BM-scannerMode")) {
+                document.body.insertAdjacentHTML('beforeend', html);
+                document.querySelector("#scannerModeBtn").classList.add("mode_on");
+                document.querySelector('#BM-scannerMode').style.zIndex = zIndex - 1;
+                document.querySelector("#BM-scannerMode").focus();
+
+                document.querySelector("#BM-scannerMode").addEventListener('blur', function () {
+                    // Bring back the focus
+                    if (document.querySelector("#BM-scannerMode")) {
+                        document.querySelector("#BM-scannerMode").focus();
+                    }
+                });
+            } else {
+                document.querySelector(".scannerModeDiv").remove();
+                document.querySelector("#scannerModeBtn").classList.remove("mode_on");
+            }
+        };
+        // Append the icon span to the scanner button
+        scannerModeBtn.innerHTML = '<svg fill="#828282" height="64px" width="64px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 60 60" xml:space="preserve" stroke="#828282"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M5,49c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v37C4,48.553,4.448,49,5,49z"></path> <path d="M55,49c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v37C54,48.553,54.448,49,55,49z"></path> <path d="M9,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C8,40.553,8.448,41,9,41z"></path> <path d="M12,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C11,40.553,11.448,41,12,41z"></path> <path d="M17,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C16,40.553,16.448,41,17,41z"></path> <path d="M20,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C19,40.553,19.448,41,20,41z"></path> <path d="M23,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C22,40.553,22.448,41,23,41z"></path> <path d="M29,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C28,40.553,28.448,41,29,41z"></path> <path d="M34,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C33,40.553,33.448,41,34,41z"></path> <path d="M37,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C36,40.553,36.448,41,37,41z"></path> <path d="M42,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C41,40.553,41.448,41,42,41z"></path> <path d="M45,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C44,40.553,44.448,41,45,41z"></path> <path d="M51,41c0.552,0,1-0.447,1-1V11c0-0.553-0.448-1-1-1s-1,0.447-1,1v29C50,40.553,50.448,41,51,41z"></path> <path d="M9,47c0.552,0,1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1v1C8,46.553,8.448,47,9,47z"></path> <path d="M12,47c0.552,0,1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1v1C11,46.553,11.448,47,12,47z"></path> <path d="M15,47c0.552,0,1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1v1C14,46.553,14.448,47,15,47z"></path> <path d="M17,45v1c0,0.553,0.448,1,1,1s1-0.447,1-1v-1c0-0.553-0.448-1-1-1S17,44.447,17,45z"></path> <path d="M20,45v1c0,0.553,0.448,1,1,1s1-0.447,1-1v-1c0-0.553-0.448-1-1-1S20,44.447,20,45z"></path> <path d="M23,45v1c0,0.553,0.448,1,1,1s1-0.447,1-1v-1c0-0.553-0.448-1-1-1S23,44.447,23,45z"></path> <path d="M27,47c0.552,0,1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1v1C26,46.553,26.448,47,27,47z"></path> <path d="M29,45v1c0,0.553,0.448,1,1,1s1-0.447,1-1v-1c0-0.553-0.448-1-1-1S29,44.447,29,45z"></path> <path d="M33,47c0.552,0,1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1v1C32,46.553,32.448,47,33,47z"></path> <path d="M35,46c0,0.553,0.448,1,1,1s1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1V46z"></path> <path d="M38,46c0,0.553,0.448,1,1,1s1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1V46z"></path> <path d="M42,47c0.552,0,1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1v1C41,46.553,41.448,47,42,47z"></path> <path d="M45,47c0.552,0,1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1v1C44,46.553,44.448,47,45,47z"></path> <path d="M48,47c0.552,0,1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1v1C47,46.553,47.448,47,48,47z"></path> <path d="M51,47c0.552,0,1-0.447,1-1v-1c0-0.553-0.448-1-1-1s-1,0.447-1,1v1C50,46.553,50.448,47,51,47z"></path> <path d="M0,5v50h60V5H0z M58,53H2V7h56V53z"></path> </g> </g></svg>';
+        // Append the scanner button to the body
+        document.body.appendChild(scannerModeBtn);
+    }
+
     /**
      * Checks whether the currently focused element within the DOM is editable or suitable for user input.
      * 
@@ -50,7 +163,7 @@ function barcodeIntercepter(args) {
     function isEditable() {
         return (focusedElement && !focusedElement.readOnly && !focusedElement.disabled &&
             (focusedElement.tagName === 'INPUT' && isEditableInputTypes(focusedElement.type)) || focusedElement.tagName === 'SELECT' ||
-                focusedElement.tagName === 'TEXTAREA' || (focusedElement.isContentEditable));
+            focusedElement.tagName === 'TEXTAREA' || (focusedElement.isContentEditable));
     }
 
 
@@ -133,7 +246,7 @@ function barcodeIntercepter(args) {
         }
     }
 
-    
+
     /**
      * Manages focus change events within the Document Object Model (DOM) to update the currently focused element.
      * Triggers the 'toggleScanner()' function to adjust the scanner state based on the focused element.
@@ -155,7 +268,7 @@ function barcodeIntercepter(args) {
         }
     }
 
-    
+
 
 
     /**
@@ -181,7 +294,7 @@ function barcodeIntercepter(args) {
                     iframe.contentWindow.addEventListener("keydown", handleInput);
                 }
             }
-        }       
+        }
 
         /**
          * Handles focus changes within iframes, updating the focused element and adjusting the scanner state.
@@ -218,7 +331,7 @@ function barcodeIntercepter(args) {
      * @param {Event} event - The event object representing the keyboard input event.
      */
     function handleInput(event) {
-        if (scannerEnabled && (event.key.length == 1 || event.key == args.endingChar)) {
+        if (!event.ctrlKey && !event.altKey && !event.metaKey && !event.shiftKey && event.key && scannerEnabled && (event.key.length == 1 || event.key == args.endingChar)) {
             if (!(event.key == args.endingChar && barcodeInput.length < args.minimumCharLength)) {
                 event.preventDefault();
             }
@@ -249,3 +362,22 @@ function barcodeIntercepter(args) {
         }
     }
 }
+    </script>
+</head>
+<body>
+    <script>
+        let args = {
+    endingChar: "Enter",
+    callback: async function (barCodeValue, isBarCode) {
+        if (isBarCode) {
+            console.log("test")
+        } else {
+            console.log(`Invalid barCode: ${barCodeValue}`);
+        }
+    },
+    minimumCharLength: 4
+};
+barcodeIntercepter(args);
+    </script>
+</body>
+</html>
